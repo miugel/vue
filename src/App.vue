910b9a461a8b3f2cc0cb79bee49408c3d3a40a90
Miguel Nicolas <miguelqnicolas@gmail.com>
2020-08-06 12:21:04 -0400

    Complete header component

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,14 @@
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
     Header
   }
-}
+};
 </script>
 
 <style>
