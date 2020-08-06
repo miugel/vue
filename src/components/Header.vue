8438dde139188c42c374aa5df06d13704705248f
Miguel Nicolas <miguelqnicolas@gmail.com>
2020-08-06 12:19:36 -0400

    Lay out components

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div class="inner-container">
+      <p>Quiz Application</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+.container {
+  background-color: maroon;
+  color: white;
+  display: flex;
+  align-items: center;
+  height: 48px;
+}
+.inner-container {
+  margin: 0 auto;
+  width: 90%;
+}
+</style>
