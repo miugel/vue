b6108e53e840b9b2cc230ccb02af9cacba19431f
Miguel Nicolas <miguelqnicolas@gmail.com>
2020-08-06 12:56:51 -0400

    Scaffold question list component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,33 @@
 <template>
   <div id="app">
     <Header />
+    <QuestionList />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import QuestionList from './components/QuestionList.vue';
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    QuestionList
   }
 };
 </script>
 
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+  }
 </style>
---
+++ src/components/QuestionList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <h1>Quiz 1 - HTML/CSS/JS Practice</h1>
+  </div>
+</template>
+
+<script>
+  import questions from '../questions.json';
+
+  export default {
+    name: 'QuestionList',
+    questions: questions
+  }
+</script>
+
+<style scoped>
+  .container {
+    margin: 0 auto;
+    width: 90%;
+  }
+
+  h1 {
+    color: #2c3e50;
+    font-size: 24px;
+    margin: 16px 0;
+  }
+
+  @media (min-width: 1024px) {
+    .container {
+      width: 921.6px;
+    }
+  }
+</style>
